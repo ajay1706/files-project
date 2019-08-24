65ba468ef0838ae5a493f0d9a908776cbc174220
ajay1706 <ajay97456@gmail.com>
2019-08-25 00:58:07 +0530

    updating nearby_friends file

--- friends.py
+++ friends.py
@@ -3,10 +3,10 @@
 # For each friend we'll save the friend to  'nearby_friends.txt'
 # hint: readLines()
 
-friends = input('Enter your 3 friends list separated by comma: '.split(','))
+friends = input('Enter your 3 friends list separated by comma:').split(',')
 
 people = open('people.txt','r')
-people_nearby = people.readlines()
+people_nearby = [line.strip() for line in people.readlines()]
 people.close()
 
 
@@ -16,10 +16,10 @@ people_nearby_set = set(people_nearby)
 
 friends_nearby_set = friends_set.intersection(people_nearby_set)
 
-nearby_friends_file = open('nearby_friends.txt','w')
+nearby_friends_file = open('nearby_friends.txt', 'w')
 
 for friend in friends_nearby_set:
     print(f'{friend} is nearby, meetup')
-    nearby_friends_file.write(f'{friend}')
+    nearby_friends_file.write(f'{friend}\n')
 
 nearby_friends_file.close()
