8cd9d37602a9676b3d4699da8ecc020669c60f91
ajay1706 <ajay97456@gmail.com>
2019-09-04 22:52:48 +0530

    adding code

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 file = open('file.txt', 'r')
 file_content = file.read()
-
 file.close()
-
 print(file_content)
 
 
-
 user_name = input("Enter username: ")
-file_writing = open('file.txt', 'w')
+my_file_writing= open('file.txt', 'w')
 
-file_writing.write(user_name)
+my_file_writing.write(user_name)
 
-file_writing.close()
+my_file_writing.close()
+
