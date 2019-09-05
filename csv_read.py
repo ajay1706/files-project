dc1abb1757bb0f91bfa720255730e1f6bd0ae688
ajay1706 <ajay97456@gmail.com>
2019-09-05 19:51:11 +0530

    adding csv

--- csv_read.py
+++ csv_read.py
@@ -1,18 +1,32 @@
-file = open('csv_data.txt' , 'r')
+file = open('csv_data.txt', 'r')
 lines = file.readlines()
 file.close()
+
 
 lines = [line.strip() for line in lines[1:]]
 
 for line in lines:
-    person_data = line.split(',')
+    person_data= line.split(',')
     name = person_data[0].title()
     age = person_data[1]
-    university = person_data[2].title()
-    degree = person_data[3].capitalize()
+    university = person_data[2].upper()
+    degree = person_data[3]
+
 
     print(f'{name} is {age}, studying {degree} at {university}')
 
 
-sample_csv_value = ' '.join(['Ajay','20','Computer Science', 'JBIET'])
+sample_csv_value = ','.join(['Rolf','23','IIT','Computing'])
 print(sample_csv_value)
+#writing into csv file
+
+
+
+
+
+
+
+
+
+
+
